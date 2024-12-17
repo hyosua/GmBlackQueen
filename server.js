2bd3393055e87f585641ccf415f55dfb222a7814
Your Name <colleterhyosua@gmail.com>
2024-12-17 23:45:27 +0100

    déclaration de la variable PORT dans le serveur pour qu'elle puisse être utilisable avec le serveur et à la fois en local

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 const app = express();
-const process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000  ;
 
 // Middleware
 app.use(cors());
